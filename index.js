8d69ef32c8751b2b9f735d1d6f56191ca58e99a9
Brance Boren <branceboren@gmail.com>
2020-05-08 13:48:18 -0600

    Configurable highWaterMark for transform stream.

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ var gen = require('generate-object-property')
 
 var CsvWriteStream = function(opts) {
   if (!opts) opts = {}
-  stream.Transform.call(this, {objectMode:true, highWaterMark:16})
+  stream.Transform.call(this, {
+    objectMode: true,
+    highWaterMark: opts.highWaterMark || 16
+  })
 
   this.sendHeaders = opts.sendHeaders !== false
   this.headers = opts.headers || null
